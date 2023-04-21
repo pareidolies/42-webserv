db2b75f99387eb7eb01b2dd15b6796b6146aee04
Yangchi <chenade0312@gmail.com>
2023-04-21 02:11:54 +0200

    -

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -46,8 +46,8 @@ int main(const int argc, const char** argv)
         // return status_code_;
     }
 
-    // TcpServer server = TcpServer("127.0.0.1", 8080);
-    // server.startListen();
+    TcpServer server = TcpServer("127.0.0.1", 8080);
+    server.startListen();
     
     return EXIT_SUCCESS;
 }
